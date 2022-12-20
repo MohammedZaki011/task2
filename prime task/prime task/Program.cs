ce5b0d70e7de567cccd35ad3a162c898c332a9e0
MohammedZaki011 <121100938+MohammedZaki011@users.noreply.github.com>
2022-12-20 14:26:59 +0200

    Add files via upload

---
+++ prime task/prime task/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool isprimenum = true;
+
+            Console.WriteLine("pls Enter the frist Number ");
+            int n1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("pls Enter the second Number ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            for (int x = n1; x <= n2; x++)
+            {
+                bool isprime = true;
+                int var = x / 2;
+
+
+                for (int y = 2; y < var; y++)
+                {
+                    if (x % y == 0)
+                    {
+                        isprime = false;
+                        break;
+
+
+                    }
+
+
+                }
+
+                if (isprime) Console.WriteLine(x + "   ");
+
+            }
+
+        }
+    }      
+}
